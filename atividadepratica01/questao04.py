8a66beee94ef1581bbf63f1c3c2c74a749980944
root-diego <contactdiego25@gmail.com>
2026-01-08 20:41:03 -0300

    atividade pratica 04

---
+++ atividadepratica01/questao04.py
@@ -0,0 +1,10 @@
+nome_produto = "Cadeira infantil"
+preco_unitario = 12.40
+quantidade = 3
+
+valor_final = preco_unitario * quantidade
+
+print("EDN Store")
+print(f"Produto: {nome_produto}")
+print(f"Preço unitário: {preco_unitario:.2f}")
+print(f"Total: {valor_final:.2f}")
