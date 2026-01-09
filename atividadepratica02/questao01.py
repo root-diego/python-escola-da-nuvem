388e0deed0c87ef4f0853fd9c010898145f981c2
root-diego <contactdiego25@gmail.com>
2026-01-09 20:11:45 -0300

    atividade pratica 02 - questao 01

---
+++ atividadepratica02/questao01.py
@@ -0,0 +1,17 @@
+valor_reais = float(input("Digite o valor em R$: "))
+
+cotacao_dolar = 5.37
+
+cotacao_euro = 6.25
+
+valor_dolar = valor_reais / cotacao_dolar
+
+valor_euro = valor_reais / cotacao_euro
+
+print(f"O valor em reais: R$ {valor_reais:.2f}")
+
+print(f"O valor em dólar: U$ {valor_dolar:.2f}")
+
+print(f"O valor em euro: € {valor_euro:.2f}")
+
+
